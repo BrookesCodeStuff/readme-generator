c46f9c9dc203a0429cc87a0a6160dae0fbc1074d
Brooke <brookepaglia@gmail.com>
2022-02-20 19:39:39 -0500

    finish README sections and logic

--- index.js
+++ index.js
@@ -6,24 +6,25 @@ const mockData = {
   title: 'Project',
   description: 'project desc',
   installation: 'install it',
-  license: 'MIT License',
+  usage: 'click things',
+  license: 'MIT',
   contributing: 'guidelines',
   tests: 'do testing',
   githubUser: 'brookescodestuff',
   email: 'email@email.com',
 };
 
-// TODO: Create an array of questions for user input
+// An array of questions for user input
 const questions = [
   {
     type: 'input',
     name: 'title',
-    message: 'What is the title of your project? (required)',
+    message: 'What is the title of your project?',
   },
   {
     type: 'input',
     name: 'description',
-    message: 'Please provide a description of your project: (required)',
+    message: 'Please provide a description of your project:',
   },
   {
     type: 'input',
@@ -31,18 +32,23 @@ const questions = [
     message: 'Provide any necessary installation instructions:',
   },
   {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide instructions and examples for use:',
+  },
+  {
     type: 'list',
     name: 'license',
     message: 'Choose your license:',
     choices: [
-      'MIT License',
-      'GNU AGPL',
-      'GPU GPL',
-      'GNU LBPL',
-      'Mozilla Public License',
-      'Apache License',
-      'Boost Software License',
-      'The Unlicense',
+      'MIT',
+      'AGPL v3',
+      'GPL v3',
+      'LBPL v3',
+      'MPL v2',
+      'Apache 2.0',
+      'BSL',
+      'Unlicense',
       'None',
     ],
     default: 0,
@@ -61,12 +67,12 @@ const questions = [
   {
     type: 'input',
     name: 'githubUser',
-    message: 'Please enter your GitHub username: (required)',
+    message: 'Please enter your GitHub username:',
   },
   {
     type: 'input',
     name: 'email',
-    message: 'Please enter your email address: (required)',
+    message: 'Please enter your email address:',
   },
 ];
 
@@ -76,7 +82,7 @@ const questions = [
 // TODO: Create a function to initialize app
 function init() {
   // inquirer.prompt(questions).then((answers) => gm(answers));
-  gm(mockData);
+  console.log(gm(mockData));
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,46 +1,101 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// Returns a license badge based on which license is passed in
+// If there is no license, returns an empty string
 function renderLicenseBadge(license) {
-  return license != 'None' ? license : '';
+  return license === 'None'
+    ? ''
+    : `![shield.io](https://img.shields.io/badge/license-${license}-green)`;
 }
 
-// // TODO: Create a function that returns the license link
-// // If there is no license, return an empty string
-// function renderLicenseLink(license) {}
+// Returns the license link
+// If there is no license, returns an empty string
+function renderLicenseLink(license) {
+  let licenseLink;
+  switch (license) {
+    case 'MIT':
+      licenseLink = 'https://choosealicense.com/licenses/mit/';
+    case 'AGPL v3':
+      licenseLink = 'https://choosealicense.com/licenses/agpl-3.0/';
+    case 'GPL v3':
+      licenseLink = 'https://choosealicense.com/licenses/gpl-3.0/';
+    case 'LBPL v3':
+      licenseLink = 'https://choosealicense.com/licenses/lgpl-3.0/';
+    case 'MPL v2':
+      licenseLink = 'https://choosealicense.com/licenses/mpl-2.0/';
+    case 'Apache 2.0':
+      licenseLink = 'https://choosealicense.com/licenses/apache-2.0/';
+    case 'BSL':
+      licenseLink = 'https://choosealicense.com/licenses/bsl-1.0/';
+    case 'Unlicense':
+      licenseLink = 'https://choosealicense.com/licenses/unlicense/';
+      break;
+    default:
+      licenseLink = '';
+  }
+  return licenseLink;
+}
 
-// // TODO: Create a function that returns the license section of README
-// // If there is no license, return an empty string
-// function renderLicenseSection(license) {}
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  return license === 'None'
+    ? ''
+    : `
+## License
+  
+This project is covered under the [${license} License](${renderLicenseLink(
+        license
+      )}). For more information regarding this license, please visit [${renderLicenseLink(
+        license
+      )}](${renderLicenseLink(license)})
+      `;
+}
 
-// // TODO: Create a function to generate markdown for README
-// function generateMarkdown(data) {
-//   return `
-//   # ${data.title}
-
-//   ## Description
-
-//   ${data.description}
-
-//   ## Table of Contents
-
-//   ${toc}
-
-// `;
-// }
-
+// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  var {
-    title,
-    description,
-    installation,
-    license,
-    contributing,
-    tests,
-    githubUser,
-    email,
-  } = data;
+  return `
+# ${data.title}
 
-  console.log(renderLicenseBadge(license));
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+${
+  data.license === 'None'
+    ? `- [Questions](#questions)`
+    : `- [License](#license)
+- [Questions](#questions)`
+}
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+${renderLicenseSection(data.license)}
+## Questions
+
+[My GitHub Profile](https://github.com/${data.githubUser})
+
+Please email ${data.email} with any questions or feedback. Thank you!
+`;
 }
 
 module.exports = generateMarkdown;
